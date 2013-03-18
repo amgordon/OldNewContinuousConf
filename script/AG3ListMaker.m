8625abd67e677f897b1f4fa91417ac9e1a656872
Alan Gordon <Alan.M.Gordon@gmail.com>
2013-03-17 20:36:57 -0700

    first pilot version

---
+++ script/AG3ListMaker.m
@@ -0,0 +1,56 @@
+y = load('wordList1092');
+testSessLength = 1092;
+testTotalLength = 1092;
+numLists = 8;
+numSess = 1;
+numItems = 1092;
+
+for i = 1:numLists;
+    thisShuffleIdx = randperm(testTotalLength);
+    wordList_all = y.wordList1092(thisShuffleIdx,1);
+    absCon_all = y.wordList1092(thisShuffleIdx,2);
+    
+    oldNew_all = [];
+    for j = 1:numSess
+        oldNew = ceil(randperm(testSessLength)/(numItems/2));
+        oldNew(oldNew>2)=0;
+        
+        wordList = cell(size(oldNew));
+        absCon = cell(size(oldNew));
+        wordList(oldNew>0) = wordList_all(1+(j-1)*numItems:(j)*numItems);
+        wordList(oldNew==0) = {'+'};
+        absCon(oldNew>0) = absCon_all(1+(j-1)*numItems:(j)*numItems);
+        absCon(oldNew==0) = {0};
+             
+        testList = [wordList' absCon' num2cell(oldNew')];
+        save (sprintf('1092_words_Test_List_%g_%g', i,j), 'testList' );
+        
+        % now make the 2nd half of lists, this time switching which is old and which
+        % new.
+        testList = [wordList' absCon' num2cell(mod(3-oldNew',3)) ];
+        save (sprintf('1092_words_Test_List_%g_%g', i+numLists, j), 'testList' );
+        
+        oldNew_all = [oldNew_all oldNew];
+        
+    end
+    oldNew_all(oldNew_all==0) = [];
+    
+    wordListStudy = wordList_all(oldNew_all==1);
+    absConStudy = absCon_all(oldNew_all==1);
+    
+    p = randperm(length(wordListStudy));
+    wordListStudy = wordListStudy(p);
+    absConStudy = absConStudy(p);
+    
+    studyList = [wordListStudy absConStudy];
+    
+    save (sprintf('546_words_Study_List_%g', i), 'studyList');
+    
+    % now make the 2nd half of lists, switching old and new.
+    wordListStudy = wordList_all(oldNew_all==2);
+    absConStudy = absCon_all(oldNew_all==2);
+    wordListStudy = wordListStudy(p);
+    absConStudy = absConStudy(p);
+    studyList = [wordListStudy absConStudy];
+    save (sprintf('546_words_Study_List_%g', i+numLists), 'studyList');
+end
